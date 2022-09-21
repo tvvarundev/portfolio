fc87387fcea6fb57e0df7d7e2e2bca2089475c59
tvvarun <tvvarun@penguin>
2022-09-21 15:32:56 +0530

    Initial Commit

---
+++ src/Components/Navbar.js
@@ -0,0 +1,22 @@
+import "./Styles/Navbar.scss";
+
+function Navbar() {
+  return (
+    <div className="navbar">
+      <div className="navbar-logo">
+        <p className="navbar-logo__name">deepu</p>
+        <p className="navbar-logo__symbol">_</p>
+      </div>
+      <div className="navbar-list">
+        <li className="navbar-list__item">Home</li>
+        <li className="navbar-list__item">Projects</li>
+        <li className="navbar-list__item">About</li>
+      </div>
+      <div className="navbar-letstalk">
+        <button className="navbar-letstalk__button">LET'S TALK</button>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
